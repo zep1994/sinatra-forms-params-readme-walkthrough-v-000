baa7cbd71e700f47c876ddc92375bb4f79c874c8
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-25 21:23:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   # Add your post route and action below
 
-      post '/food' do
-        params.to_s
-      end
+    post '/food' do
+      "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+    end
 end
